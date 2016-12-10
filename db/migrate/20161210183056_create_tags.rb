1acb407aec252bc7ff18af81e8914ec7c515a4a8
Antonio Borrero Granell <me@antoniobg.com>
2016-12-10 10:37:25 -0800

    Simplify tags table

--- db/migrate/20161210183056_create_tags.rb
+++ db/migrate/20161210183056_create_tags.rb
@@ -1,12 +1,9 @@
 class CreateTags < ActiveRecord::Migration[5.0]
   def change
     create_table :tags do |t|
-      t.string :name,  null: false, index: true
-      t.string :value, null: false, index: true
+      t.string :name,  null: false, index: { unique: true }
 
       t.timestamps
     end
-
-    add_index :tags, [:name, :value], unique: true
   end
 end
