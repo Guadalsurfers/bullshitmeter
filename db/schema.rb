2ae35c91bb8f97d8fb0926a3ef679fcce9da3e78
Antonio Borrero Granell <me@antoniobg.com>
2016-12-09 23:13:00 -0800

    Fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -40,4 +40,5 @@ ActiveRecord::Schema.define(version: 20161210070349) do
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
+
 end
