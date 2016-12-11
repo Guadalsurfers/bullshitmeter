d6f8fd1d15e15e0fac08bbd058a06f2ad10d3844
Miguel Carranza <miguel@mindsnacks.com>
2016-12-10 19:18:20 -0800

    Adding more columns to user

---
+++ db/migrate/20161210234549_add_basic_columns_to_user.rb
@@ -0,0 +1,9 @@
+class AddBasicColumnsToUser < ActiveRecord::Migration[5.0]
+  def change
+    add_column  :users, :first_name,  :string, null: false
+    add_column  :users, :last_name,   :string, null: false
+    add_column  :users, :google_id,   :string, null: false
+
+    add_index :users, :google_id, unique:true
+  end
+end
