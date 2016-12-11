bb1d7156454410b13af7a635ab0b1771ae246cbe
Antonio Borrero Granell <me@antoniobg.com>
2016-12-10 23:18:29 -0800

    Add sidekiq admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+
   namespace :api do
     get 'articles/', to: 'articles#show', as: :article
     resources :votes, only: [:create]
