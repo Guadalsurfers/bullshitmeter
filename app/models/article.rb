b42203dc076756e427114a86c536a0879125a90c
Antonio Borrero Granell <me@antoniobg.com>
2016-12-20 18:26:14 -0800

    Create Medium model and add relationship with articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,10 @@
 class Article < ApplicationRecord
+  belongs_to :medium, optional: true
+
+  has_many :votes, dependent: :delete_all
+
   has_and_belongs_to_many :authors, join_table: :article_authors
   has_and_belongs_to_many :tags,    join_table: :article_tags
-
-  has_many :votes,           dependent: :delete_all
 
   validates :bs_index, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1 }
   validates :url,      presence: true, uniqueness: true
